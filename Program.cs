a533d86e1781e7bd496118c76a82af25502ff179
paiiiigggeeee <91573998+paiiiigggeeee@users.noreply.github.com>
2024-02-07 12:45:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailAlerts
+{
+    class Program
+    {
+        // Create a logger for use in this class
+        public static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+
+            if (log.IsInfoEnabled) log.Info("The ConnectionString is test");
+            
+        }
+    }
+}
